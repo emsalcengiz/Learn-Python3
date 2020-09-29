3fc109bc860393f2893ffd32d6d4e936f7429486
Emsal Cengiz (ETIYA) <emsal.cengiz@etiya.com>
2020-09-29 23:08:28 +0300

    update files

--- random_file.py
+++ random_file.py
@@ -1,6 +1,6 @@
 import random
 
-isimler = ["Emsal 🤬 ","Göksel 😎 "]
+isimler = ["Göksel 🤬 ","Emsal 😎 "]
 taraf = ["sağ", "sol"]
 
 print(str(isimler[random.getrandbits(1)]) + " " + str(taraf[random.getrandbits(1)]) + "daki kahveyi içer. ☕️")
