349d082bf52dc29ad3ab2d66c6dfd4bf67678ea5
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-08 10:28:00 +0300

    updated

--- requests_module/exm.py
+++ requests_module/exm.py
@@ -5,10 +5,11 @@ import click
 # url = "https://github.com/emsalcengiz"
 
 @click.command()
-@click.option('--url', help="web sitesi girilir.", required=True, default="https://github.com/emsalcengiz")
+@click.option('--url', help="web sitesi girilir.", required=True,
+              default="https://github.com/emsalcengiz", show_default=True)
 def check_website(url):
     try:
-        print("girdiğiniz site adresi:", url)
+        #print("girdiğiniz site adresi:", url)
         r = requests.get(url)
         if(r.status_code == 200):
             print("başarılı")
