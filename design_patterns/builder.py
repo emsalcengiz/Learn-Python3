ab90767c9b84d39c8f6691986459e4819c90a4a6
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-14 11:47:45 +0300

    updated

---
+++ design_patterns/builder.py
@@ -0,0 +1,13 @@
+"""
+Ne zaman ihtiyaç duyarız ?
+" Eğer çok fazla parametre alan bir objeniz var ise bu objeyi oluştururken constructor’ında bir çok “null” parametre geçmeye başladıysanız."
+
+"Eğer objenizi oluştururken birden çok constructor’ı varsa ve hangisini kullanacağınız konusu net değilse,
+ bu objeyi oluşturmak için minimum hangi alanları doldurmanız gerektiği konusunda kafa karışıklığınız oluşuyorsa."
+
+"Nesnenin oluşturulduktan sonra değişmez (immutable) olmasını istiyorsanız"
+
+
+
+
+"""
