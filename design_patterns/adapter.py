d5d476cdab2afde399adf2de14f5c1ebfb40af2a
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-14 11:43:13 +0300

    updated

--- design_patterns/adapter.py
+++ design_patterns/adapter.py
@@ -10,3 +10,39 @@ Bir sınıfın arayüzünü müşterilerin istediği arayüze dönüştürülür
 Adaptör, sınıfların uyumsuz ara birimler nedeniyle aksi halde olmayan birlikte çalışmasına izin verir. mevcut bir sınıfı bir arayüzle sarar.
 
 """
+
+from typing import Protocol
+
+
+class RowingBoat(Protocol):
+    def row(self):
+        ...
+
+
+class FishingBoat:
+    """A boat to go fishing"""
+
+    def sail(self):
+        print("The fishing boat is sailing")
+
+
+class Captain:
+    def __init__(self, rowing_boat: RowingBoat):
+        self.rowing_boat = rowing_boat
+
+    def row(self):
+        self.rowing_boat.row()
+
+
+class FishingBoatAdapter(RowingBoat):
+    def __init__(self):
+        self.fishing_boat = FishingBoat()
+
+    def row(self):
+        self.fishing_boat.sail()
+
+
+if __name__ == "__main__":
+    captain = Captain(FishingBoatAdapter())
+
+    captain.row()
