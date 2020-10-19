d484a94118c8a1b1b160d92f455879bcbf01c453
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-19 14:21:33 +0300

    updated

--- tutorial_two/game.py
+++ tutorial_two/game.py
@@ -2,7 +2,16 @@ game = [[0,0,0,],
         [0,0,0],
         [0,0,0],]
 
-print(type(game))
-for row in game:
-    print(row)
+
+game[0][1] = 1
+
+# print(type(game))
+print("   a  b  c")
+# count = 0
+for count, row in enumerate(game):
+    print(count, row)
+    # count= count+1
+    # for item in row:
+    #     print(item)
+
 
