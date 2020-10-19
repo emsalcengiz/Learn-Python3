0ca754812e1695204efd2a9a83fd79c59f6c67b7
Emsal Cengiz <emsal_cengiz@hotmail.com>
2020-10-19 16:45:07 +0300

    updated

--- tutorial_two/game.py
+++ tutorial_two/game.py
@@ -3,15 +3,31 @@ game = [[0,0,0,],
         [0,0,0],]
 
 
-game[0][1] = 1
+def game_board(player=0, row=0, column=0, just_display=False):
+    # print(type(game))
+    print("   0  1  2")
+    if not just_display:
+        game[row][column]= player
+    # count = 0
+    for count, row in enumerate(game):
+        print(count, row)
+        # count= count+1
+        # for item in row:
+        #     print(item)   
 
-# print(type(game))
-print("   a  b  c")
-# count = 0
-for count, row in enumerate(game):
-    print(count, row)
-    # count= count+1
-    # for item in row:
-    #     print(item)
+
+
+game_board(just_display=True)
+print("  ---------")
+game_board(player=1, row=2, column=1)
+# game[0][1] = 1
+# game_board()
+
+# def addition(x, y):
+#     return x+y
+
+
+# z = addition("hey"," there")
+# print(z)
 
 
---
+++ tutorial_two/test.py
@@ -0,0 +1,36 @@
+#bi miktar eğlendim
+x = 1
+def test():
+    x = 2
+test()
+print(x)
+
+
+x = 1
+def test():
+    global x
+    x = 2
+test()
+print(x)
+
+
+x = [1]
+def test():
+    x = [2]
+test()
+print(x)
+
+
+x = [1]
+def test():
+    global x
+    x = [2]
+test()
+print(x)
+
+
+x = [1]
+def test():
+    x[0] = 2
+test()
+print(x)
